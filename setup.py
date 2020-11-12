27b17d89ad54129a5118d52dec3504395ee58647
Minar Ashiq Tishan <tishan150@gmail.com>
2020-11-12 08:10:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       description="Generate German word list from Wiki with meaning as a CSV",
       version="1.0.0",
       url="https://github.com/MinarAshiqTishan/Wiki-De-En-Gen",
-      install_requires=["dictcc", "pandas", "wikipedia-api"],
+      install_requires=["dict.cc.py", "pandas", "wikipedia-api"],
       author='Minar Ashiq Tishan',
       packages=find_packages(),
      )
